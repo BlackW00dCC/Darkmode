48de8c92936ba9bf20eea2b53dd62ecc5a997906
BlackW00dCC <92838846+BlackW00dCC@users.noreply.github.com>
2021-10-26 13:32:27 +0700

    Add files via upload

---
+++ html กับ darkmode/script.js
@@ -0,0 +1,27 @@
+
+
+function dk_mode(){
+
+document.body.style.backgroundColor="black";
+document.body.style.color = "yellow";
+}
+
+
+
+
+/*
+############ ช่วยจำ JS#########################################
+var a = [ 1 , 2, 3, 4];
+//var b = a.concat([1,2,3,4,5,6]);
+/*console.log(b)
+b.pop()
+console.log(b)
+b.push(300)
+console.log(b)
+b.shift() //unshift > "+"
+console.log(b)
+b.splice(3,3,20)//(index,del index no =0,newdata)
+console.log(b)
+var c = a.slice(1);//ปิดการแสดง index ที่ระบุและแสดง index ถัดไป หรือใส่ขอบเขต a.slice(0,2)
+console.log(c)
+*/
